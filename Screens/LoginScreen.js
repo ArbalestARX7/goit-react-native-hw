665fc48185aa4118052196d95c17b50b3d260303
Nikita <evildeus666@gmail.com>
2023-08-02 14:33:20 +0300

    login & register update

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,44 +1,149 @@
-import { StyleSheet, Text, View } from "react-native";
-import BackGroundIMG from "../components/BackGroundIMG";
-import LoginForm from "../components/LoginForm";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ImageBackground,
+  Pressable,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import Input from "../components/Input";
+import { useState } from "react";
+export const bgImg = require("../Images/PhotoBG.jpg");
 
-const LoginScreen = () => {
+export default function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordHidden, setPasswordHidden] = useState(true);
+
+  const handleSubmit = () => {
+    console.log(`email: ${email}`, `password: ${password}`);
+  };
+
+  const handlePressShowButton = () => {
+    setPasswordHidden((hidden) => !hidden);
+  };
+
   return (
-    <>
-      <BackGroundIMG>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+      >
         <View style={styles.container}>
-          <View style={styles.registerBg}>
-            <Text style={styles.title}>Увійти</Text>
-            <LoginForm />
-          </View>
-        </View>
-      </BackGroundIMG>
-    </>
-  );
-};
+          <ImageBackground source={bgImg} style={styles.background}>
+            <View style={styles.menuContainer}>
+              <Text style={styles.title}>Увійти</Text>
 
-export default LoginScreen;
+              <View>
+                <Input
+                  placeholder="Адреса електронної пошти"
+                  inputMode="email"
+                  value={email}
+                  onChangeText={setEmail}
+                />
+                <Input
+                  placeholder="Пароль"
+                  inputMode="text"
+                  value={password}
+                  secureTextEntry={passwordHidden}
+                  onChangeText={setPassword}
+                />
+
+                <Pressable style={styles.passwShowWrapper}>
+                  <Text
+                    style={styles.passwShowText}
+                    onPress={handlePressShowButton}
+                  >
+                    {!passwordHidden ? "Приховати" : "Показати"}
+                  </Text>
+                </Pressable>
+
+                <Pressable style={styles.button}>
+                  <Text style={styles.buttonText} onPress={handleSubmit}>
+                    Увійти
+                  </Text>
+                </Pressable>
+              </View>
+
+              <Pressable style={styles.registrationLinkContainer}>
+                <Text style={styles.registrationText}>
+                  Немає акаунту?{" "}
+                  <Text style={styles.registrationTextUnderlined}>
+                    Зареєструватися
+                  </Text>
+                </Text>
+              </Pressable>
+            </View>
+          </ImageBackground>
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: "column-reverse",
+    width: "100%",
+    height: "100%",
+    color: "#FFFFFF",
+    backgroundColor: "#FF6C00",
+    display: "flex",
+    justifyContent: "center",
   },
-
-  registerBg: {
+  menuContainer: {
+    backgroundColor: "#FFFFFF",
+    position: "absolute",
+    width: "100%",
+    height: 489,
+    bottom: 0,
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
-    backgroundColor: "#fff",
-    flex: 0.6,
     alignItems: "center",
-    paddingLeft: 16,
-    paddingRight: 16,
   },
-
   title: {
-    fontWeight: 500,
-    fontSize: 30,
-    textAlign: "center",
     marginTop: 32,
+    marginBottom: 16,
+    fontSize: 30,
+    lineHeight: 35,
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#FF6C00",
+    height: 50,
+    width: 343,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+    marginTop: 43,
+  },
+  passwShowText: {
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#1B4371",
+  },
+  passwShowWrapper: {
+    top: -34,
+    left: 255,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+  },
+  registrationLinkContainer: {
+    marginTop: 16,
+  },
+  registrationText: {
+    fontSize: 16,
+    lineHeight: 19,
+    textAlign: "center",
+    color: "#1B4371",
+  },
+  registrationTextUnderlined: {
+    textDecorationLine: "underline",
+  },
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
   },
 });
--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,62 +1,177 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import RegisterForm from "../components/RegisterForm";
-import BackGroundIMG from "../components/BackGroundIMG";
-import AddSVG from "../Images/add.svg";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ImageBackground,
+  Pressable,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
+import Input from "../components/Input";
+export const bgImg = require("../Images/PhotoBG.jpg");
 
-const RegistrationScreen = () => (
-  <>
-    <BackGroundIMG>
-      <View style={styles.container}>
-        <View style={styles.registerBg}>
-          <View style={styles.photoPlace}>
-            <AddSVG width={25} height={25} style={styles.addSVG} />
-          </View>
-          <Text style={styles.title}>Реєстрація</Text>
-          <RegisterForm />
+export default function RegistrationScreen() {
+  const [login, setLogin] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordHidden, setPasswordHidden] = useState(true);
+
+  const handleSubmit = () => {
+    console.log(`login: ${login}`, `email: ${email}`, `password: ${password}`);
+  };
+
+  const handlePressShowButton = () => {
+    setPasswordHidden((hidden) => !hidden);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+      >
+        <View style={styles.container}>
+          <ImageBackground source={bgImg} style={styles.background}>
+            <View style={styles.menuContainer}>
+              <View style={styles.avatarContainer}>
+                <View style={styles.avatar} />
+                <Pressable>
+                  <AntDesign
+                    name="pluscircleo"
+                    size={24}
+                    color="#FF6C00"
+                    style={styles.addAvatarBtn}
+                  />
+                </Pressable>
+              </View>
+
+              <Text style={styles.title}>Реєстрація</Text>
+
+              <View>
+                <Input
+                  placeholder="Логін"
+                  inputMode="text"
+                  value={login}
+                  onChangeText={setLogin}
+                />
+                <Input
+                  placeholder="Адреса електронної пошти"
+                  inputMode="email"
+                  value={email}
+                  onChangeText={setEmail}
+                />
+                <Input
+                  placeholder="Пароль"
+                  inputMode="text"
+                  value={password}
+                  secureTextEntry={passwordHidden}
+                  onChangeText={setPassword}
+                />
+
+                <Pressable style={styles.passwShow}>
+                  <Text
+                    style={styles.passwShowText}
+                    onPress={handlePressShowButton}
+                  >
+                    {!passwordHidden ? "Приховати" : "Показати"}
+                  </Text>
+                </Pressable>
+
+                <Pressable style={styles.button}>
+                  <Text style={styles.buttonText} onPress={handleSubmit}>
+                    Зареєстуватися
+                  </Text>
+                </Pressable>
+              </View>
+
+              <Pressable style={styles.loginLinkContainer}>
+                <Text style={styles.loginText}>Вже є акаунт? Увійти</Text>
+              </Pressable>
+            </View>
+          </ImageBackground>
         </View>
-      </View>
-    </BackGroundIMG>
-  </>
-);
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: "column-reverse",
+    width: "100%",
+    height: "100%",
+    color: "#FFFFFF",
+    backgroundColor: "#FF6C00",
+    display: "flex",
+    justifyContent: "center",
   },
-
-  registerBg: {
+  menuContainer: {
+    backgroundColor: "#FFFFFF",
+    position: "absolute",
+    width: "100%",
+    height: 549,
+    bottom: 0,
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
-    backgroundColor: "#fff",
-    flex: 0.7,
     alignItems: "center",
-    paddingLeft: 16,
-    paddingRight: 16,
   },
-
-  title: {
-    fontWeight: 500,
-    fontSize: 30,
-    textAlign: "center",
-    marginTop: 92,
-  },
-
-  photoPlace: {
+  avatarContainer: {
+    position: "absolute",
+    backgroundColor: "#F6F6F6",
+    borderRadius: 16,
+    top: -60,
     width: 120,
     height: 120,
-    backgroundColor: "#F6F6F6",
-    position: "absolute",
-    top: -60,
-    borderRadius: 16,
   },
-
-  addSVG: {
+  avatar: {},
+  addAvatarBtn: {
     position: "absolute",
-    bottom: 14,
+    bottom: -105,
     right: -12.5,
+    backgroundColor: "#ffffff",
+    borderRadius: 50,
+  },
+  title: {
+    marginTop: 92,
+    marginBottom: 16,
+    fontSize: 30,
+    lineHeight: 35,
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#FF6C00",
+    height: 50,
+    width: 343,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+    marginTop: 43,
+  },
+  passwShowText: {
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#1B4371",
+  },
+  passwShow: {
+    top: -34,
+    left: 255,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+  },
+  loginLinkContainer: {
+    marginTop: 16,
+  },
+  loginText: {
+    fontSize: 16,
+    lineHeight: 19,
+    textAlign: "center",
+    color: "#1B4371",
+  },
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
   },
 });
-
-export default RegistrationScreen;
